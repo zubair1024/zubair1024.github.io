526de8f6d02089b6e5d3cd4daeafe9bb8cef41f8
Zubair Ahmed <zubair1024@gmail.com>
2017-09-25 00:13:57 +0400

    adding kendo components

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,65 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
     $(".button-collapse").sideNav();
+    var data = [
+      "Albania",
+      "Andorra",
+      "Armenia",
+      "Austria",
+      "Azerbaijan",
+      "Belarus",
+      "Belgium",
+      "Bosnia & Herzegovina",
+      "Bulgaria",
+      "Croatia",
+      "Cyprus",
+      "Czech Republic",
+      "Denmark",
+      "Estonia",
+      "Finland",
+      "France",
+      "Georgia",
+      "Germany",
+      "Greece",
+      "Hungary",
+      "Iceland",
+      "Ireland",
+      "Italy",
+      "Kosovo",
+      "Latvia",
+      "Liechtenstein",
+      "Lithuania",
+      "Luxembourg",
+      "Macedonia",
+      "Malta",
+      "Moldova",
+      "Monaco",
+      "Montenegro",
+      "Netherlands",
+      "Norway",
+      "Poland",
+      "Portugal",
+      "Romania",
+      "Russia",
+      "San Marino",
+      "Serbia",
+      "Slovakia",
+      "Slovenia",
+      "Spain",
+      "Sweden",
+      "Switzerland",
+      "Turkey",
+      "Ukraine",
+      "United Kingdom",
+      "Vatican City"
+    ];
+
+    //create AutoComplete UI component
+    $("#countries").kendoAutoComplete({
+      dataSource: data,
+      filter: "startswith",
+      placeholder: "Select country...",
+      separator: ", "
+    });
   });
 })(jQuery);
